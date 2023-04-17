9ecda1817957f509c8ac76cb11b85d1180b4c3aa
Võ Nguyễn Phát <vonguyenphat012@gmail.com>
2023-04-17 18:07:19 +0700

    select-movie-and-showtime

--- spring_cinema_app/src/main/java/com/example/demo/controller/TicketController.java
+++ spring_cinema_app/src/main/java/com/example/demo/controller/TicketController.java
@@ -1,10 +1,21 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.ticket.Ticket;
+import com.example.demo.service.ticket.ITicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cinema/ticket")
 public class TicketController {
-
+    @Autowired
+    ITicketService ticketService;
+    @GetMapping("/list-ticket")
+    public List<Ticket> getAllTicket(){
+        return ticketService.findAllTicket();
+    }
 }
