a11444ecbfac310a842d2a8a877a9896ef9cadb6
congdanh2601 <congdanhdp1420@gmail.com>
2023-04-17 19:00:10 +0700

    test pull

--- spring_cinema_app/src/main/java/com/example/demo/service/customer/ICustomerService.java
+++ spring_cinema_app/src/main/java/com/example/demo/service/customer/ICustomerService.java
@@ -1,4 +1,10 @@
 package com.example.demo.service.customer;
 
+import com.example.demo.model.customer.Customer;
+
+import java.util.List;
+
 public interface ICustomerService {
+    List<Customer> getAllCustomer();
+
 }
--- spring_cinema_app/src/main/java/com/example/demo/service/impl/customer/CustomerService.java
+++ spring_cinema_app/src/main/java/com/example/demo/service/impl/customer/CustomerService.java
@@ -1,8 +1,25 @@
 package com.example.demo.service.impl.customer;
 
+import com.example.demo.model.customer.Customer;
+import com.example.demo.repository.customer.ICustomerRepository;
 import com.example.demo.service.customer.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomerService implements ICustomerService {
+    private ICustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(ICustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+
+    public List<Customer> getAllCustomer() {
+        return customerRepository.findAll();
+    }
 }
