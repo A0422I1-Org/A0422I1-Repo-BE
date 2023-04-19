2d6ade8370dd8509bc2e6549158385b4b0985009
Võ Nguyễn Phát <vonguyenphat012@gmail.com>
2023-04-19 20:02:08 +0700

    PhatVN 19/04/2023

--- spring_cinema_app/src/main/java/com/example/demo/repository/ticket/IShowTimeRepository.java
+++ spring_cinema_app/src/main/java/com/example/demo/repository/ticket/IShowTimeRepository.java
@@ -2,14 +2,18 @@ package com.example.demo.repository.ticket;
 
 import com.example.demo.model.ticket.ShowTime;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
-public interface IShowTimeRepository extends JpaRepository<ShowTime,Integer> {
-//    @Query("select ShowTime from ShowTime where ShowTime.movie.id =: idMovie")
-     List<ShowTime> findShowTimeByMovieId(Integer id);
+@Transactional
+public interface IShowTimeRepository extends JpaRepository<ShowTime, Integer> {
+    @Modifying
+    @Query(value = "Select st.id, st.date,st.end_time, st.sold_out,st.start_time ,st.is_delete,st.movie_id From show_time st Where st.movie_id =:id",nativeQuery = true)
+    List<ShowTime> findShowTimeByMovieId(@Param("id") int id);
 }
