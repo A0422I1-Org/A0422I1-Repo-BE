45764cab95a6e73a1ea4ecbdc7f858527d71020d
Phamtheduyet123 <duyetpham322@gmail.com>
2023-04-24 21:20:15 +0700

    update version final

--- spring_cinema_app/src/main/java/com/example/demo/model/ticket/Ticket.java
+++ spring_cinema_app/src/main/java/com/example/demo/model/ticket/Ticket.java
@@ -17,7 +17,7 @@ public class Ticket {
     private String id;
     @NotNull
     private Double price;
-    private Date book_datetime;
+    private Date bookDateTime;
     @NotNull
     private Boolean status;
     private Boolean isDelete;
@@ -30,7 +30,7 @@ public class Ticket {
 //    private Employee employee;
     @ManyToOne
     @JoinColumn(name = "showtime_id")
-    private ShowTime showTime;
+    private ShowTime showtime;
 
     @ManyToOne
     @JoinColumn(name = "chair_room_id")
@@ -39,14 +39,14 @@ public class Ticket {
     public Ticket() {
     }
 
-    public Ticket(String id, Double price, Date book_datetime, Boolean status, Boolean isDelete, Customer customer, ShowTime showTime, ChairRoom chairRoom) {
+    public Ticket(String id, @NotNull Double price, Date bookDateTime, @NotNull Boolean status, Boolean isDelete, Customer customer, ShowTime showtime, ChairRoom chairRoom) {
         this.id = id;
         this.price = price;
-        this.book_datetime = book_datetime;
+        this.bookDateTime = bookDateTime;
         this.status = status;
         this.isDelete = isDelete;
         this.customer = customer;
-        this.showTime = showTime;
+        this.showtime = showtime;
         this.chairRoom = chairRoom;
     }
 }
